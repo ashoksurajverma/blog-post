a46e67c92e3a48a7da8a9c10900f466d873a2421
Suraj Verma <ashoksurajverma@gmail.com>
2023-04-19 13:50:47 +0530

    WIP

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
 const express = require("express");
+const path = require("path");
 const mongoose = require("mongoose");
+const { v4: uuidv4 } = require('uuid');
+const multer = require("multer");
+const bodyParser = require("body-parser");
 const feedRoutes = require("./routes/feed.route");
 const authRoutes = require("./routes/auth.route");
 const app = express();
 const PORT = 8080;
 
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+      cb(null, 'images');
+  },
+  filename: function(req, file, cb) {
+      cb(null, uuidv4())
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+app.use(
+  multer({ storage: storage, fileFilter: fileFilter }).single('image')
+);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.use("/auth", authRoutes);
 app.use("/feed", feedRoutes);
